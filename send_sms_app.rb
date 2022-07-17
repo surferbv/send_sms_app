8fa7b1b6a266a87fdd92b46ec948b312f355d13e
surferbv <surferbv@gmail.com>
2022-07-17 12:55:09 -0700

    yes!

--- send_sms_app.rb
+++ send_sms_app.rb
@@ -9,27 +9,28 @@ require 'twilio-ruby'
 require 'uri'
 require 'net/http'
 require 'openssl'
+require 'pry'
 
 account_sid = 'ACa2f13a6ea3a7422120717dac5481e28d'
-auth_token = '3c6b003b04eb668aa41e731b26f72f0b' 
+auth_token = 'a7743c848bc2934d9a6be9d22e6e4432' 
 
 def fetch_it(caller, url, key = '')
-	response = Struct.new( :code, :response)
-	parsed_json = response.new('','')
+	res_struct = Struct.new( :code, :response)
+	parsed_json_struct = res_struct.new('','')
 	uri_url = URI(url)
 	https = Net::HTTP.new(uri_url.host, uri_url.port)
 	https.use_ssl = true
 	request = Net::HTTP::Get.new(uri_url)
 	request["X-API-KEY"] = key if !key.empty?
-	response = https.request(request)
-	if response.is_a?(Net::HTTPSuccess)
-		parsed_json.code = response.code
-		parsed_json.response = JSON.parse(response.body, :symbolize_names => true)
+	res_struct = https.request(request)
+	if res_struct.is_a?(Net::HTTPSuccess)
+		parsed_json_struct.code = res_struct.code
+		parsed_json_struct.response = JSON.parse(res_struct.body, :symbolize_names => true)
 	else
-		parsed_json.code = response.code
-		parsed_json.response = caller
+		parsed_json_struct.code = res_struct.code
+		parsed_json_struct.response = caller
 	end
-	parsed_json
+	parsed_json_struct
 end
 
 if account_sid && auth_token
@@ -70,13 +71,15 @@ if account_sid && auth_token
 	# weather
 	caller = "Weather call "
 	url = "https://api.weather.gov/gridpoints/MTR/88,80/forecast?units=si"
-	parsed_json = fetch_it(caller, url, key)
+	parsed_json_struct = fetch_it(caller, url, key)
 
-	if parsed_json.code = '200'
+	if parsed_json_struct.code = '200'
 		result += "\n"
-		result += "#{parsed_json.response[:properties][:periods][0][:detailedForecast]}"
+		parsed_json_struct.response.key?("properties") ? result : result += "properties missing"
+		parsed_json_struct.response.key?("periods") ? result : result += " periods missing"
+		result += "#{parsed_json_struct.response[:properties][:periods][0][:detailedForecast]}" if !result.include?("missing")
 	else
-		result += parsed_json.response + parsed_json.code
+		result += parsed_json_struct.response + parsed_json_struct.code
 	end
 	
 	# twillio message
